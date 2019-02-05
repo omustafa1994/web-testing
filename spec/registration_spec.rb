619205d779d6f6c6586cfd8ed77ac58a50e5d33c
omustafa1994 <omustafa1994@gmail.com>
2019-02-05 11:51:21 +0000

    initial push

---
+++ spec/registration_spec.rb
@@ -0,0 +1,18 @@
+require 'spec_helper'
+
+describe 'testing the demoqa automation form' do 
+
+  before(:all) do
+    @driver = QatoolsForm.new
+    @driver = visit_practise_form
+    @url = 'https://www.toolsqa.com/automation-practice-form'
+  end
+
+  context 'testing the positive pahts for the form' do 
+
+    it 'should land on the registration demo form page' do 
+      expect(@driver.current_url).to eq @url
+    end
+
+  end
+end
